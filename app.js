7e74d2123429d48d354ee66827ece26057430287
kailash4 <kailash@bigcity.in>
2020-08-28 10:27:16 +0530

    Exoress get request done

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// var logger = require('morgan');
+var bodyParser = require("body-parser");
 var cors = require('cors');
 var mongoose = require('mongoose');
+var url  = 'mongodb+srv://user1:1234@cluster0.wyolt.mongodb.net/CRUD?retryWrites=true&w=majority';
 
-mongoose.connect('mongodb://localhost/bigcity-crud', {
+mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
@@ -14,31 +16,41 @@ mongoose.connect('mongodb://localhost/bigcity-crud', {
   .catch((err) => console.error(err));
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/employees');
 
 var app = express();
-
+app.listen(9000,()=>{
+  console.log("server started");
+})
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(cors());
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+
+// app.use(logger('dev'));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+// app.use('/', indexRouter);
+app.use('/employees', usersRouter);
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next)=> {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next)=> {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
